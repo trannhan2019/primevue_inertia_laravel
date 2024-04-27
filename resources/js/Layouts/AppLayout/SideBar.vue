f9dbd641e8c4655fe36c7efec152957d987d3426
nhanphuong <trannhan.s3@gmail.com>
2024-04-27 11:17:42 +0700

    first commit

---
+++ resources/js/Layouts/AppLayout/SideBar.vue
@@ -0,0 +1,7 @@
+<script setup>
+import SidebarMenu from "./SidebarMenu.vue";
+</script>
+
+<template>
+    <SidebarMenu />
+</template>
---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,15 @@
+<script>
+import AppLayout from "@/Layouts/AppLayout/Index.vue";
+export default {
+    layout: AppLayout,
+};
+</script>
+
+<script setup>
+import { Head } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head title="Test" />
+    <h3>Test</h3>
+</template>
